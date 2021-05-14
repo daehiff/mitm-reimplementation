cce56a65642c1bbd090889d92d62b0ed498ef126
daehiff <winderl13@gmail.com>
2021-05-14 22:44:39 +0200

    added erro when running out of depth

--- src/python_lib.cpp
+++ src/python_lib.cpp
@@ -4,6 +4,7 @@
 #include "xtensor/xarray.hpp"
 
 #define FORCE_IMPORT_ARRAY
+
 #include "xtensor-python/pyarray.hpp"
 #include "xtensor-python/pyvectorize.hpp"
 
@@ -17,19 +18,19 @@
 
 namespace py = pybind11;
 
-std::string mitm_algorithm(xt::pyarray<std::complex<double>> &unitary, int max_depth, int m = 1, bool verbose = true)
-{
+std::string mitm_algorithm(xt::pyarray<std::complex<double>> &unitary, int max_depth, int m = 1, bool verbose = true) {
     if (unitary.shape()[0] != unitary.shape()[1])
         throw std::runtime_error("Requires a unitary of equal shape");
-    int numQubits = (int)std::log2(unitary.shape()[0]);
+    int numQubits = (int) std::log2(unitary.shape()[0]);
     if (std::pow(2, numQubits) != unitary.shape()[0])
         throw std::runtime_error("Provided unitary has no shape of 2**n");
     auto circ = mitmAlgorithm(unitary, max_depth, numQubits, m, verbose);
-    return circ.toqcFormat()    ;
+    if (circ.gates.size() == 0)
+        return "";
+    return circ.toqcFormat();
 }
 
-PYBIND11_MODULE(mitm, m)
-{
+PYBIND11_MODULE(mitm, m) {
     xt::import_numpy();
     m.doc() = R"pbdoc(
         An example xtensor extension
